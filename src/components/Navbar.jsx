44b1e57ab3f83d7ed9cf0cc0de69d2a72371d4d3
rsbassi9 <rajvir.bassi95@gmail.com>
2023-07-27 06:27:40 -0700

    Added a personal Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,11 +6,24 @@ import { NavLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed tp-0 z-20 bg-primary`}
     >
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto"></div>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/" // Points to the top of the page
+          className="flex items-center gap-2" // Special ract router DOM link so we can give it a click functiion
+          onClick={() => {
+            setActive(""); // Keeps track of where we are on the page
+            window.scrollTo(0, 0); // Scroll to the top of the page
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </Link>
+      </div>
     </nav>
   );
 };
