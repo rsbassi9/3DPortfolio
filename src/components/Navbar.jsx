490adfe4745d0f1526fcb7ee329407937cf37164
rsbassi9 <rajvir.bassi95@gmail.com>
2023-07-31 06:41:48 -0700

    chagned logo, updating tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Rajvir Bassi<span className="sm:block hidden"> | Portfolio</span>
+          </p>
         </Link>
       </div>
     </nav>
