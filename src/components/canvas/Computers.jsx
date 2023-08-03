79d02368aa8a784854624424a6f4eb85ee033040
rsbassi9 <rajvir.bassi95@gmail.com>
2023-08-03 06:27:18 -0700

    tested different 3D models. updated loading animation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,9 +9,22 @@ const Computers = () => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.35} groundColor="black" />
       <pointLight intensity={1} />
-      <primitive object={computer.scene} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        //castShadow
+        //shdow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
