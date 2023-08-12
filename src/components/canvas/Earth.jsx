8b02022da9dfc880e66abbd189a512fc26e53017
rsbassi9 <rajvir.bassi95@gmail.com>
2023-08-11 17:27:11 -0700

    Completed implementing Earth Model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
@@ -16,9 +16,15 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameLoop="demand"
+      frameloop="demand"
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
-      camera={{}}
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
+      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -27,11 +33,11 @@ const EarthCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-
         <Earth />
+        <Preload all />
       </Suspense>
     </Canvas>
   );
 };
 
-export default Earth;
+export default EarthCanvas;
