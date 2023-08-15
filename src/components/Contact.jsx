5aedc8bb42a1518307631008b5706091a090d910
rsbassi9 <rajvir.bassi95@gmail.com>
2023-08-15 05:30:47 -0700

    Completed Contact form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+//template_fzg4jim
+// service_u3jw1it
+//KViQ4_OqOzTY9rbNy
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,49 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emialjs
+      .send(
+        "service_u3jw1it",
+        "template_fzg4jim",
+        {
+          from_name: form.name,
+          to_name: "Rajvir",
+          from_email: form.email,
+          to_email: "rajvir.bassi95@gmail.com",
+          message: form.message,
+        },
+        "KViQ4_OqOzTY9rbNy"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.log(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
